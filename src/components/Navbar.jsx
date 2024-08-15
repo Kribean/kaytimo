006f8702df763b8100039b6aba396030b7622f19
Kribean <caraibe.simulation@gmail.com>
2024-08-15 22:36:00 +0200

    update: add font

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,19 @@
+import { Pacifico } from "next/font/google";
+const pacifico = Pacifico({
+  subsets: ["latin"],
+  weight: "400", // Définir le poids de la police, 400 pour normal
+});
+
 export const Navbar = () => {
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">Kay Ti Mo</a>
+        <a href="./" className={pacifico.className+" btn btn-ghost  text-xl "}>Kay Ti Mo</a>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <a>Toutes les Activités</a>
+            <a href="./activities">Toutes les Activités</a>
           </li>
           <li>
             <a href="./account">Mon Compte</a>
