42de72a4e4ea236241bf02146c2e993e02ec4d8e
Kribean <caraibe.simulation@gmail.com>
2024-08-22 01:10:05 +0200

    feat: add suspense

--- src/app/activities/page.js
+++ src/app/activities/page.js
@@ -1,48 +1,59 @@
 "use client"
-// pages/account.js
- 
-import { useSearchParams } from 'next/navigation'
+
+import React, { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
 import { ButtonTheme } from '@/components/ButtonTheme';
 import { CardActivity } from '@/components/CardActivity';
 import { Footer } from '@/components/Footer';
 import { Navbar } from '@/components/Navbar';
-import React from 'react';
 
 const ActivitiesPage = () => {
-  const searchParams = useSearchParams()
- 
-  const search = searchParams.get('query')
-  console.log(search,"koooooo")
+  return (
+    <div className="flex flex-col min-h-screen bg-base-200">
+      <Navbar/>
+      <div className='flex flex-col w-full justify-center items-center pt-[200px]'>
+        <h1 className='text-5xl font-bold'>Les activités</h1>
+      </div>
+      
+      <Suspense fallback={<div>Loading...</div>}>
+        <ActivitiesContent />
+      </Suspense>
+
+      <Footer/>
+    </div>
+  );
+};
+
+const ActivitiesContent = () => {
+  const searchParams = useSearchParams();
+  const search = searchParams.get('query');
+  console.log(search, "koooooo");
+
   const sectionElement = [
-    {acti:'Activité Nocturne',imgsrc:"../image/nightlife.png",indx:1},
-    {acti:'Restaurant',imgsrc:"../image/restaurant.png",indx:2},
-    {acti:'Plages',imgsrc:"../image/hiking.png",indx:3},
-    {acti:'Shopping',imgsrc:"../image/shopping.png",indx:4},
-    {acti:'Activité Atypique',imgsrc:"../image/insolite.png",indx:5}]
+    { acti: 'Activité Nocturne', imgsrc: "../image/nightlife.png", indx: 1 },
+    { acti: 'Restaurant', imgsrc: "../image/restaurant.png", indx: 2 },
+    { acti: 'Plages', imgsrc: "../image/hiking.png", indx: 3 },
+    { acti: 'Shopping', imgsrc: "../image/shopping.png", indx: 4 },
+    { acti: 'Activité Atypique', imgsrc: "../image/insolite.png", indx: 5 }
+  ];
 
   return (
-    <div className=" flex flex-col min-h-screen bg-base-200">
-<Navbar/>
-<div className='flex flex-col w-full justify-center items-center pt-[200px]'>
-<h1 className='text-5xl font-bold'>Les activités</h1>
-</div>
-<section className='flex flex-col m-4'>
-  <h2 className='text-xl font-bold'>Filtre</h2>
-  <div className='flex flex-wrap gap-2 items-center w-full'>
-  {
-    sectionElement.map((e,index)=>{return <ButtonTheme key={index} acti={e.acti} indx={e.indx}/>})
-  }
-  </div>
+    <>
+      <section className='flex flex-col m-4'>
+        <h2 className='text-xl font-bold'>Filtre</h2>
+        <div className='flex flex-wrap gap-2 items-center w-full'>
+          {sectionElement.map((e, index) => (
+            <ButtonTheme key={index} acti={e.acti} indx={e.indx} />
+          ))}
+        </div>
+      </section>
 
-
-</section>
-<section className='flex flex-wrap gap-4'>
-<CardActivity/>
-<CardActivity/>
-<CardActivity/>
-</section>
-<Footer/>
-    </div>
+      <section className='flex flex-wrap gap-4'>
+        <CardActivity />
+        <CardActivity />
+        <CardActivity />
+      </section>
+    </>
   );
 };
 
